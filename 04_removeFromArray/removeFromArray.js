3239b2527c87c758e082b204984d7c9474b25948
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-19 17:35:42 +0100

    Finished Solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, val) {
+    // Get values to remove
+    let toRemove = Object.values(arguments);
+    toRemove.shift(); // Removes the array from argument list
 
+    console.log(toRemove);
+    console.log(arr);
+    console.log(`-----------------`)
+    
+    // Loop through array and remove values
+    toRemove.forEach(val => {
+        let index = arr.indexOf(val);
+
+        if(index >= 0)
+            arr.splice(index, 1);
+    })
+
+    return arr;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3, 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
