a3d2d76e1b4c4091ff82acf8eeba28c9d6964988
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-19 18:00:00 +0100

    Completed Solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+const ftoc = function (temp) {
+  let converted = Math.round((temp - 32) * (5 / 9) * 10) / 10;
 
+  return converted;
 };
 
-const ctof = function() {
+const ctof = function (temp) {
+  let converted = Math.round((temp * (9 / 5) + 32) * 10) / 10;
 
+  return converted;
 };
 
 // Do not edit below this line
