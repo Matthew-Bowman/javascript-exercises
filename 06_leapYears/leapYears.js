31641a1b3f0df520acdf4dffa036735ffb7c9f5b
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-19 17:49:08 +0100

    Completed Solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // Is a Century
+    if (year % 100 == 0) {
+        if (year % 400 == 0)
+            return true;
+        else
+            return false;
+    } 
+    // Not a Century
+    else {
+        if (year % 4 == 0)
+            return true;
+        else
+            return false;
+    }
 };
 
 // Do not edit below this line
